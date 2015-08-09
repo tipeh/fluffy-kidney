8866caec7ad1d776445b89ca2c15a2909f739b69
lezzar <walezz89@gmail.com>
2015-08-09 16:44:48 +0200

    The website is now poorly handling post requests

--- functional_tests.py
+++ functional_tests.py
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('<h1>').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
 
         # She is invited to enter a to-do item straight away
@@ -33,15 +33,15 @@ class NewVisitorTest(unittest.TestCase):
         # is tying fly-fishing lures)
         input_box.send_keys('Buy peacock feathers')
 
+
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         input_box.send_keys(Keys.ENTER)
 
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers',(row.text for row in rows))
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
