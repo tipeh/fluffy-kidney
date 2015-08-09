3e9f08584eac3056af6419b6f528b5a6db3e88f0
lezzar <walezz89@gmail.com>
2015-08-09 13:12:27 +0200

    Using unittest instead

---
+++ functional_tests.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the tests')
+
+        # She is invited to enter a to-do item straight away
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # The page updates again, and now shows both items on her list
+
+if __name__ == 'main':
+    unittest.main(warnings='ignore')
