3094f9580e77ee9f48ad18231cb8179b56276e03
lezzar <walezz89@gmail.com>
2015-08-11 22:53:01 +0200

    Added new list after submitting

--- lists/views.py
+++ lists/views.py
@@ -7,10 +7,12 @@ def home_page(request):
     if request.method == 'POST':
         new_text_item = request.POST["item_text"]
         Item.objects.create(text=new_text_item)
-        return redirect("/")
+        return redirect('/lists/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
+
+def view_list(request):
     items = Item.objects.all()
-
-    return render(request, 'home.html', {'items':items})
+    return render(request, 'list.html', {'items': items})
 
 # Create your views here.
--- superlists/urls.py
+++ superlists/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', 'lists.views.home_page', name = 'home')
+    url(r'^$', 'lists.views.home_page', name = 'home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),
     #url(r'^admin/', include(admin.site.urls)),
 ]
