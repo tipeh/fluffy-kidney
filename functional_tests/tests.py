5ab8e1c47fbd091717f55461cc59fa7d48b5d6c1
lezzar <walezz89@gmail.com>
2015-08-19 17:51:43 +0200

    Styling functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -99,6 +99,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn(edith_item_to_send_2, page_text)
 
 
+    def test_styling_and_layout(self):
+        #Edith goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        #She notices the input is nicely centered
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(inputbox.location["x"] + inputbox.size["width"]/2,
+                               512,
+                               delta=5
+                               )
+
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+                    inputbox.location['x'] + inputbox.size['width'] / 2,
+                    512,
+                    delta=5)
+
     def check_for_row_in_list_table(self,row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
